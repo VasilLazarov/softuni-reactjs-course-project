ba2c63e28074718b3846db1b9d063cc3095568cb
Vasil Lazarov <105485479+VasilLazarov@users.noreply.github.com>
2025-04-08 00:08:03 +0300

    Added react router
    Added react router and make index route to Home component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router";
+
+import Home from "./components/home/Home";
 import "./App.css";
 
 function App() {
     return (
         <>
             <h1>Cleaned Project</h1>
+
+            <Routes>
+                <Route path="/" element={<Home />}/>
+            </Routes>
         </>
     );
 }
