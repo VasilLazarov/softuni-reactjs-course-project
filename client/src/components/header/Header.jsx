cb0a26eef2697b6e8243d22b5924e5ffde0d632b
Vasil Lazarov <105485479+VasilLazarov@users.noreply.github.com>
2025-04-09 05:17:43 +0300

    Add header and some css
    Added header navigation and css files.

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,5 +1,32 @@
+import { Link } from "react-router";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
 export default function Header() {
-   return (
-       <h2>Header</h2>
-   );
+    library.add(fas);
+    return (
+        <header>
+            <h1>
+                <Link to="/">VL Book Library</Link>
+            </h1>
+            <nav>
+                {/* Show correct links when user is logged and not logged */}
+                <div>
+                    <Link to="/books">Books</Link>
+                    <Link to="/books/create">Create Book</Link>
+                    <Link to="/userInfo">
+                        <FontAwesomeIcon icon="fa-solid fa-user" />
+                    </Link>
+                    <Link to="/logout">Logout</Link>
+                </div>
+                <div>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                    
+                </div>
+            </nav>
+        </header>
+    );
 }
