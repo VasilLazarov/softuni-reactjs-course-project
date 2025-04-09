c677e50fd9c889a5926adf77515ad4f21d95a361
Vasil Lazarov <105485479+VasilLazarov@users.noreply.github.com>
2025-04-09 06:21:41 +0300

    Update home page
    Added jsx elements in home component and create css for home page.

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,5 +1,12 @@
 export default function Home() {
-   return (
-       <h2>Home</h2>
-   );
+    return (
+        <div id="home-box">
+            <div className="home-title">
+                <h1>Welcome to VL Book Library</h1>
+            </div>
+            <img src="/images/book.png" alt="book" />
+
+            {/* Create dinamic show newest or best rating books */}
+        </div>
+    );
 }
