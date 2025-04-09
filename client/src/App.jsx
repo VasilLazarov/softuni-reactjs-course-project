6dd535b4866a9d19c97426135bec9fe5039d964e
Vasil Lazarov <105485479+VasilLazarov@users.noreply.github.com>
2025-04-09 05:18:32 +0300

    Add routes
    Added routes for navigation links.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,26 @@
 import { Route, Routes } from "react-router";
 
 import Home from "./components/home/Home";
+import Header from "./components/header/Header";
 import "./App.css";
 
 function App() {
     return (
-        <>
-            <h1>Cleaned Project</h1>
+        <div id="page">
+            <Header />
 
-            <Routes>
-                <Route path="/" element={<Home />}/>
-            </Routes>
-        </>
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/books" element={<Home />} />
+                    <Route path="/books/create" element={<Home />} />
+                    <Route path="/userInfo" element={<Home />} />
+                    <Route path="/login" element={<Home />} />
+                    <Route path="/register" element={<Home />} />
+                    <Route path="/logout" element={<Home />} />
+                </Routes>
+            </main>
+        </div>
     );
 }
 
